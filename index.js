fa5986b12cbf1b521c056aeedf835f2962f1ac4f
Jasper Zonneveld <jasper@swis.nl>
2018-10-26 17:25:30 +0200

    Add webpack rules

---
+++ index.js
@@ -0,0 +1,29 @@
+module.exports = (api, projectOptions) => {
+    api.chainWebpack(webpackConfig => {
+        const pluginOptions = projectOptions.pluginOptions ? projectOptions.pluginOptions.svgSprite || {} : {};
+
+        const dir = api.resolve(pluginOptions.dir || 'src/assets/icons');
+
+        webpackConfig.module
+            .rule('svg-sprite')
+            .test(pluginOptions.test || /\.(svg)(\?.*)?$/)
+            .include
+            .add(dir)
+            .end()
+            .use('svg-sprite-loader')
+            .loader('svg-sprite-loader')
+            .options(pluginOptions.loaderOptions || {
+                extract: true,
+                spriteFilename: 'img/icons.[hash:8].svg',
+            });
+
+        webpackConfig
+            .plugin('svg-sprite')
+            .use(require('svg-sprite-loader/plugin'), [pluginOptions.pluginOptions || { plainSprite: true }]);
+
+        webpackConfig.module
+            .rule('svg')
+            .exclude
+            .add(dir);
+    });
+};
